6ab58da300d0657414a3a352981192634aa949f8
Driijko <driijko@gmail.com>
2023-11-30 10:07:00 -0500

    global CSS: animations.

---
+++ src/App.svelte
@@ -0,0 +1,9 @@
+<!-- SCRIPTS ///////////////////////////////////////////////// -->
+<script>
+  // IMPORTS -------------------------------------------------
+  import OpeningRouter from "./components/1-site/routers/OpeningRouter.svelte";
+
+</script>
+
+<!-- MARKUP //////////////////////////////////////////////////// -->
+<OpeningRouter />
