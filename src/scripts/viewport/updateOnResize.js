2d18af061ccf2632e4665f2f8d15f987d457cfea
Driijko <driijko@gmail.com>
2023-11-30 13:10:08 -0500

    scripts: updateOnResize.

---
+++ src/scripts/viewport/updateOnResize.js
@@ -0,0 +1,7 @@
+import { updateBreakpoint } from "../../dynamic/breakpoint";
+
+export default function updateOnResize() {
+  window.addEventListener("resize", ()=> {
+    updateBreakpoint();
+  });
+};
