56fa2f38956287b7196df34274c51574c8954baf
Driijko <driijko@gmail.com>
2023-12-01 11:06:24 -0500

    Page.

--- src/components/1-site/routers/ContentRouter.svelte
+++ src/components/1-site/routers/ContentRouter.svelte
@@ -2,18 +2,29 @@
 <script>
   // IMPORTS --------------------------------------------
   import { currentPage } from "../../../dynamic/currentPage";
+  import Page from "./Page.svelte";
 
 </script>
 
 <!-- MARKUP ///////////////////////////////////////////// -->
 {#if $currentPage === "home"}
-
+  <Page>
+    <div class="fill"></div>
+  </Page>
 {:else if $currentPage === "catalogue"}
-
+  <Page>
+    <div class="fill"></div>
+  </Page>
 {:else if $currentPage === "about"}
-
+  <Page>
+    <div class="fill"></div>
+  </Page>
 {:else if $currentPage === "design"}
-
+  <Page>
+    <div class="fill"></div>
+  </Page>
 {:else if $currentPage === "product"}
-
+  <Page>
+    <div class="fill"></div>
+  </Page>
 {/if}
--- src/components/1-site/routers/Page.svelte
+++ src/components/1-site/routers/Page.svelte
@@ -1,2 +1,25 @@
 <!-- SCRIPTS ////////////////////////////////// -->
-<script></script>
+<script>
+  // IMPORTS ----------------------------------------
+  import { setPageExit } from "../../../dynamic/currentPage";
+
+  let fadeOut = false;
+
+  setPageExit(()=> fadeOut = true, 1000);
+
+</script>
+
+<!-- MARKUP ////////////////////////////////////// -->
+<div class="fill" class:fadeOut>
+  <slot />
+</div>
+
+<!-- STYLES ////////////////////////////////////// -->
+<style>
+div {
+  animation: fade-in 1s ease-out forwards;
+}
+.fadeOut {
+  animation: fade-out 1s ease-out forwards;
+}
+</style>
