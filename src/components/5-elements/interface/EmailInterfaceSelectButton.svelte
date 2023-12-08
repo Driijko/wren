158a15ab7b8cea564bdb040ab7a84e2d67a4ac8b
Driijko <driijko@gmail.com>
2023-12-08 12:45:20 -0500

    EnvelopeIcon.

---
+++ src/components/5-elements/interface/EmailInterfaceSelectButton.svelte
@@ -0,0 +1,17 @@
+<!-- SCRIPTS //////////////////////////////////// -->
+<script>
+  // IMPORTS -------------------------------------
+  import InterfaceSelectButton from "./InterfaceSelectButton.svelte";
+  import CompassIcon from "../../6-icons/CompassIcon.svelte";
+
+</script>
+
+<!-- MARKUP ///////////////////////////////////// -->
+<InterfaceSelectButton 
+  interfaceSelect={{
+    site: "siteMenu",
+    siteMenu: "email",
+  }}
+>
+  <CompassIcon />
+</InterfaceSelectButton>
