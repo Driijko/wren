3ba94b578ea25348f65303ab728e69236553e33f
Driijko <driijko@gmail.com>
2023-12-08 10:19:47 -0500

    SiteMenuInterface.

---
+++ src/components/1-site/interface/interfaces/SiteMenu.svelte
@@ -0,0 +1,13 @@
+<!-- SCRIPTS //////////////////////////////////////// -->
+<script>
+  // IMPORTS ---------------------------------------------
+  import { breakpoint } from "../../../../dynamic/breakpoint";
+  import SiteHeader from "../../../4-structures/SiteHeader.svelte";
+
+</script>
+
+<div>
+  {#if $breakpoint === "mobile"}
+    <SiteHeader />
+  {/if}
+</div>
---
+++ src/components/1-site/interface/levels/SiteMenuInterface.svelte
@@ -0,0 +1,14 @@
+<!-- SCRIPTS //////////////////////////////////////// -->
+<script>
+  // IMPORTS ------------------------------------------
+  import { currentInterfaceSiteMenu } from "../../../../dynamic/interface";
+
+</script>
+
+{#if $currentInterfaceSiteMenu === "navigation"}
+  <p>Navigation</p>
+{:else if $currentInterfaceSite === "email"}
+  <p>Email</p>
+{:else if $currentInterfaceSite === "design"}
+  <p>Web Design</p>
+{/if}
