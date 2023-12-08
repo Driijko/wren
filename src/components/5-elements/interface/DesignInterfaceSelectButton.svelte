2d2ecf7b7160c6580f9d63e7f59058c6d11e5cd4
Driijko <driijko@gmail.com>
2023-12-08 13:04:34 -0500

    PenToSquareIcon.

---
+++ src/components/5-elements/interface/DesignInterfaceSelectButton.svelte
@@ -0,0 +1,17 @@
+<!-- SCRIPTS //////////////////////////////////// -->
+<script>
+  // IMPORTS -------------------------------------
+  import InterfaceSelectButton from "./InterfaceSelectButton.svelte";
+  import EnvelopeIcon from "../../6-icons/EnvelopeIcon.svelte";
+
+</script>
+
+<!-- MARKUP ///////////////////////////////////// -->
+<InterfaceSelectButton 
+  interfaceSelect={{
+    site: "siteMenu",
+    siteMenu: "design",
+  }}
+>
+  <EnvelopeIcon />
+</InterfaceSelectButton>
