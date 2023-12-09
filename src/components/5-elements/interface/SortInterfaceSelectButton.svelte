5754b15f4eede086d9a5a2d1365e5b650323f36e
Driijko <driijko@gmail.com>
2023-12-08 22:06:23 -0500

    SortInterfaceSelectButton.

---
+++ src/components/5-elements/interface/SortInterfaceSelectButton.svelte
@@ -0,0 +1,16 @@
+<!-- SCRIPTS //////////////////////////////////// -->
+<script>
+  // IMPORTS -------------------------------------
+  import InterfaceSelectButton from "./InterfaceSelectButton.svelte";
+  import SortIcon from "../../6-icons/SortIcon.svelte";
+
+</script>
+
+<!-- MARKUP ///////////////////////////////////// -->
+<InterfaceSelectButton 
+  interfaceSelect={{
+    site: "sort",
+  }}
+>
+  <SortIcon />
+</InterfaceSelectButton>
