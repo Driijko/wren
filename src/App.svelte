12eb3f00e4a1a3d061ec66fa00297467c449c7f0
Driijko <driijko@gmail.com>
2023-12-10 19:56:15 -0500

    App: add viewportSetup and remove updateOnResize.

--- src/App.svelte
+++ src/App.svelte
@@ -2,27 +2,12 @@
 <script>
   // IMPORTS -------------------------------------------------
   import OpeningRouter from "./components/1-site/routers/OpeningRouter.svelte";
-  import updateOnResize from "./scripts/viewport/updateOnResize";
+  import viewportSetup from "./scripts/viewport/viewportSetup";
 
   // EVENT HANDLERS ---------------------------------------------
-  updateOnResize();
+  viewportSetup();
 
 </script>
 
 <!-- MARKUP //////////////////////////////////////////////////// -->
-<!-- <OpeningRouter /> -->
-<div class="vp">
-  <div class="uarr"></div>
-</div>
-
-<!-- STYLES ////////////////////////////////////////////////// -->
-<style>
-div.vp {
-  background-color: blue;
-}
-div.uarr {
-  background-color: red;
-  width: var(--wpx);
-  height: var(--hpx);
-}
-</style>
+<OpeningRouter />
