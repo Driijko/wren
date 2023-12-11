9bb871ff9cff5aee4265b5fbba459f1b2462f94b
Driijko <driijko@gmail.com>
2023-12-11 12:58:05 -0500

    Design.

---
+++ src/components/1-site/interface/interfaces/Design.svelte
@@ -0,0 +1,25 @@
+<!-- SCRIPTS ///////////////////////////////////// -->
+<script>
+  import { breakpoint } from "../../../../dynamic/breakpoint";
+  import PageLink from "../../../5-elements/links/PageLink.svelte";
+
+</script>
+
+<!-- MARKUP ////////////////////////////////////// -->
+<div class="site-menu-interface">
+  <img src={ 
+      $breakpoint === "mobile" ?
+      "./logos/dreeko-logo-small.png" :
+      "./logos/dreeko-logo-medium.png"
+    }
+    alt="Dreeko"
+  />
+  <p>
+    Dreeko is a web developer, designer and animator living in Toronto,
+    Canada.
+  </p>
+  <p>
+    To learn more about how this site was made, check out the 
+    <PageLink page="design">web-design page</PageLink>.
+  </p>
+</div>
