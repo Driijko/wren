e967081fbaa25f0d5762f2b92dda71a3af90c1ac
Driijko <driijko@gmail.com>
2023-12-11 12:59:27 -0500

    SiteMenuInterface: add Design.

--- src/components/1-site/interface/levels/SiteMenuInterface.svelte
+++ src/components/1-site/interface/levels/SiteMenuInterface.svelte
@@ -4,21 +4,15 @@
   import { currentInterfaceSiteMenu } from "../../../../dynamic/interface";
   import Nav from "../interfaces/Nav.svelte";
   import Email from "../interfaces/Email.svelte";
+  import Design from "../interfaces/Design.svelte";
 
 </script>
 
+<!-- MARKUP /////////////////////////////////////// -->
 {#if $currentInterfaceSiteMenu === "navigation"}
   <Nav />
 {:else if $currentInterfaceSiteMenu === "email"}
   <Email />
 {:else if $currentInterfaceSiteMenu === "design"}
-  <p>Web Design</p>
+  <Design />
 {/if}
-
-<style>
-.site-menu-interface {
-  flex: 1;
-  background-color: hsl(0, 0%, 20%);
-  color: white;
-}
-</style>
