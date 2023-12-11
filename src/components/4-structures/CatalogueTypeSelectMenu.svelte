89a74203d767f278e08a1b3318197546a5e294a4
Driijko <driijko@gmail.com>
2023-12-11 17:47:43 -0500

    CatalogueTypeSelectMenu.

---
+++ src/components/4-structures/CatalogueTypeSelectMenu.svelte
@@ -0,0 +1,31 @@
+<!-- SCRIPTS /////////////////////////////////// -->
+<script>
+  // IMPORTS ------------------------------------
+  import { catalogueType } from "../../dynamic/catalogueDisplay";
+  import CatalogueTypeSelectButton 
+  from "../5-elements/catalogue/CatalogueTypeSelectButton.svelte";
+
+</script>
+
+<menu>
+  <li>
+    <CatalogueTypeSelectButton type="series">
+      Series
+    </CatalogueTypeSelectButton>
+  </li>
+  <li>
+    <CatalogueTypeSelectButton type="books">
+      Books
+    </CatalogueTypeSelectButton>
+  </li>
+  <li>
+    <CatalogueTypeSelectButton type="compilations">
+      Compilations
+    </CatalogueTypeSelectButton>
+  </li>
+  <li>
+    <CatalogueTypeSelectButton type="subjects">
+      Subjects
+    </CatalogueTypeSelectButton>
+  </li>
+</menu>
