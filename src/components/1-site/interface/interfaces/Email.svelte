56d04a347365cf4a357a98c75b216a0575b75417
Driijko <driijko@gmail.com>
2023-12-11 12:23:26 -0500

    TwitterIcon.

---
+++ src/components/1-site/interface/interfaces/Email.svelte
@@ -0,0 +1,27 @@
+<!-- SCRIPTS //////////////////////////////////////// -->
+<script>
+  // IMPORTS ------------------------------------
+  
+</script>
+
+<!-- MARKUP ///////////////////////////////////// -->
+<menu>
+  <li>
+    Contact me at:<br/>
+    <address>
+      <a href="mailto:nelsymlit@tutanota.com">
+        nelsymlit@tutanota.com
+      </a>
+    </address>
+  </li>
+  <li>
+    Subscribe to my newsletter:<br/>
+    <form>
+      <input type="email" placeholder="enter your email" />
+      <button type="submit">Submit</button>
+    </form>
+  </li>
+  <li>
+
+  </li>
+</menu>
