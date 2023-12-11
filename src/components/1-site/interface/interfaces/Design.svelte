497edb4b8dbbb8d5160a1e210ef64f01240ad385
Driijko <driijko@gmail.com>
2023-12-11 17:32:33 -0500

    static: catalogue.

--- src/components/1-site/interface/interfaces/Design.svelte
+++ src/components/1-site/interface/interfaces/Design.svelte
@@ -19,7 +19,36 @@
     Canada.
   </p>
   <p>
-    To learn more about how this site was made, check out the 
-    <PageLink page="design">web-design page</PageLink>.
+    <span>
+      To learn more about how this site was made, check out the 
+      <PageLink page="design">web-design page</PageLink>.
+    </span>
   </p>
 </div>
+
+<!-- STYLES ///////////////////////////////////////// -->
+<style>
+div {
+  display: flex;
+  flex-direction: column;
+  padding: calc(var(--hpx) * 0.1) 0%;
+}
+div > * {
+  padding: 0% calc(var(--wpx) * 0.1);
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+div :global(a) {
+  display: inline;
+}
+img {
+  object-fit: contain;
+  width: 100%;
+}
+p {
+  line-height: 1.5;
+}
+</style>
