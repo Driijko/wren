4f5e8f9ba67835850a59e59a2ed660af13928592
Driijko <driijko@gmail.com>
2023-12-10 19:45:40 -0500

    scripts: viewport: updateOnResize: add viewportCSS.

--- src/scripts/viewport/updateOnResize.js
+++ src/scripts/viewport/updateOnResize.js
@@ -1,5 +1,9 @@
 import { updateBreakpoint } from "../../dynamic/breakpoint";
+import viewportCSS from "./viewportCSS";
 
 export default function updateOnResize() {
-  window.addEventListener("resize", updateBreakpoint);
+  window.addEventListener("resize", ()=> {
+    updateBreakpoint();
+    viewportCSS();
+  });
 };
