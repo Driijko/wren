115f677890e4f7d046a5396824e89a093fe0b38a
Driijko <driijko@gmail.com>
2023-12-11 12:44:15 -0500

    Email: add layout styling.

--- src/components/1-site/interface/interfaces/Email.svelte
+++ src/components/1-site/interface/interfaces/Email.svelte
@@ -18,7 +18,7 @@
   <li>
     Subscribe to my newsletter:<br/>
     <form>
-      <input type="email" placeholder="enter your email" />
+      <input type="email" placeholder="enter your email" /><br/>
       <button type="submit">Submit</button>
     </form>
   </li>
@@ -30,3 +30,34 @@
     </a>
   </li>
 </menu>
+
+<!-- STYLES ///////////////////////////////////// -->
+<style>
+menu {
+  display: flex;
+  flex-direction: column;
+  padding: calc(var(--hpx) * 0.04) 0%;
+}
+li {
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: calc(var(--hpx) * 0.02);
+}
+form {
+  display: flex;
+  flex-direction: column;
+}
+input {
+  border: 4px solid black;
+  width: calc(var(--wpx) * 0.4);
+  height: calc(var(--hpx) * 0.05);
+}
+menu :global(svg) {
+  /* border: 4px solid white; */
+  width: calc(var(--wpx) * 0.07);
+  height: calc(var(--wpx) * 0.07);
+}
+</style>
