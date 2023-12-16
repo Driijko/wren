b8ae618160f23d6e6cac2decddec74807c787af1
Driijko <driijko@gmail.com>
2023-12-16 18:12:53 -0500

    CatalogueMainTypeButton.

---
+++ src/components/5-elements/catalogue/CatalogueMainTypeButton.svelte
@@ -0,0 +1,17 @@
+<!-- SCRIPTS /////////////////////////////////////////// -->
+<script>
+  // IMPORTS -------------------------------------
+  import { catalogueMainType } from "../../../dynamic/catalogueDisplay";
+
+  // PROPS ------------------------------------------
+  export let type;
+
+</script>
+
+<!-- MARKUP ////////////////////////////////////////// -->
+<button 
+  on:click={()=> catalogueMainType.set(type)}
+  class:current={$catalogueMainType === type}
+>
+  <slot />
+</button>
