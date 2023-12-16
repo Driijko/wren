f0ccf3f40598241a0df0f58056f3c6b792c7967b
Driijko <driijko@gmail.com>
2023-12-16 18:43:04 -0500

    static: compilations: add type property to all compilations.

--- src/static/compilations.js
+++ src/static/compilations.js
@@ -1,6 +1,7 @@
 const compilations = [
   {
     id: 8,
+    type: "compilation",
     title: "Mirror World Complete",
     titleSplit: ["Mirror World", " Complete"],
     description: "Emmett is a nice guy but can't seem to keep a girlfriend. One night he enters a world inside his mirror in which all women not only have huge tits and asses but who also can't seem to keep their hands off of him. Even his sister can't resist his charms. But soon Emmett realizes that he can't control when he enters and leaves this mirror world and that there's more going on than meets the eye.",
@@ -15,6 +16,7 @@ const compilations = [
   },
   {
     id: 7,
+    type: "compilation",
     title: "Bimbo Stories Bundle 3",
     titleSplit: ["Bimbo Stories", " Bundle 3"],
     description: "Another hypnotizing, breast-expanding bundle has arrived for Nel Symington's beloved Bimbo Stories series. Peek into the life of an executive before her mind shrinks and her bust grows. Watch as an indignant woman turns sex-crazy just from a pack of bubblegum. Enjoy as a secretary tries to uncover a conspiracy but ends up on her knees. Want more? Need more? Bimbo Stories are here for you.",
@@ -29,6 +31,7 @@ const compilations = [
   },
   {
     id: 6,
+    type: "compilation",
     title: "Hypnosis Erotica 14-Pack!",
     titleSplit: ["Hypnosis Erotica", " 14-Pack!"],
     description: "Well over 100k words of mind-draining libido-pumping bra-busting erotica swirl the pages of this collection that stretches over 3 years of Nel's library. What a way to get caught up! Just dive in and fall under the spell. You can't resist.",
@@ -43,6 +46,7 @@ const compilations = [
   },
   {
     id: 5,
+    type: "compilation",
     title: "Daddy Daughter Incest 5-Pack",
     titleSplit: ["Daddy Daughter", " Incest 5-Pack"],
     description: "All of my books about fathers and the incestuous relationships they have with their daughters to date! Over 42k words all showcasing this special taboo relationship in one convenient discounted package!",
@@ -57,6 +61,7 @@ const compilations = [
   },
   {
     id: 4,
+    type: "compilation",
     title: "Nurse Mommy Complete",
     titleSplit: ["Nurse Mommy", " Complete"],
     description: "All four entries of Nurse Mommy are now available as one convenient collection! Jason gets injured and runs into trouble when his massively busty mother insists on taking care of him. Things get both heated and complicated as another gorgeous nurse gets hired and enters the mix. There are two busty MILFs but only one Jason, who will he choose?",
@@ -71,6 +76,7 @@ const compilations = [
   },
   {
     id: 3,
+    type: "compilation",
     title: "Hypno Sister Complete",
     titleSplit: ["Hypno Sister", " Complete"],
     description: "All five entries of Hypno Sister are now available as one convenient collection! Join Philip Hughes as he finds his uncle's old amulet, accidentally hypnotizes his beautiful huge-titted sister, and then gets addicted to building an eager hypno harem. All five books at bulk pricing! ",
@@ -85,6 +91,7 @@ const compilations = [
   },
   {
     id: 2,
+    type: "compilation",
     title: "Incest Inn: Bundle",
     titleSplit: ["Incest Inn:", " Bundle"],
     description: "Finally the Incest Inn series gets a bundle to call its own! From an ashamed mother who wants her son to dominate her to a young man who gets his wish of a free use family to a grandfather obsessed with his mega-busty granddaughter, there's plenty of naughty taboo treats to be found in the old and mysterious Home Nest Hotel! Come take a room, it's very discreet.",
@@ -99,6 +106,7 @@ const compilations = [
   },
   {
     id: 1,
+    type: "compilation",
     title: "Bimbo Stories Bundle 2", 
     titleSplit: ["Bimbo Stories", " Bundle 2"],
     description: "This bra-busting bundle is stuffed with shocking transformations, lewd hypnotism, and - most importantly of all - huge-titted cock-hungry Bimbo babes. From a patient and devious psychiatrist to a cross between Willy Wonka's factory and the Playboy mansion to an uptight professor and more, these stories all head in one head-bobbing direction: *slurrrrp*!",
@@ -113,6 +121,7 @@ const compilations = [
   },
   {
     id: 0,
+    type: "compilation",
     title: "Bimbo Stories Bundle",
     titleSplit: ["Bimbo Stories", " Bundle"],
     description: "Nel Symington's first Bimbo Bundle arrives! Enjoy 9 mini stories and 3 short stories of perverted hypnotism, breast expansion, and Bimbofication in this collection of varied and lewd tales of transformation. Settle in and let your mind go blank. ",
