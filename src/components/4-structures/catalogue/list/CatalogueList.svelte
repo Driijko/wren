eb30b2ce29e5809220d89a93c31869a30b6e518a
Driijko <driijko@gmail.com>
2023-12-16 18:54:43 -0500

    CatalogueList.

--- src/components/4-structures/catalogue/list/CatalogueList.svelte
+++ src/components/4-structures/catalogue/list/CatalogueList.svelte
@@ -5,11 +5,18 @@
 
   // PROPS ------------------------------------------
   export let items;
-
   
 </script>
 
 <!-- MARKUP ////////////////////////////////////////// -->
 <ul class="reg-scroll">
-
+  {#if $vpwidth < 500}
+    {#each items as item}
+      <li>{item.title}, narrow</li>
+    {/each}
+  {:else if $vpwidth >= 500}
+    {#each items as item}
+      <li>{item.title}, wide</li>
+    {/each}
+  {/if}
 </ul>
