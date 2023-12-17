74c3f3ca95bf6040a069c72046e9bde5203c041f
Driijko <driijko@gmail.com>
2023-12-16 21:09:32 -0500

    CatalogueListItemHeader: add narrow and wide styles.

--- src/components/4-structures/catalogue/list/item/CatalogueListItemHeader.svelte
+++ src/components/4-structures/catalogue/list/item/CatalogueListItemHeader.svelte
@@ -8,7 +8,10 @@
 </script>
 
 <!-- MARKUP //////////////////////////////////////// -->
-<header>
+<header 
+  class:narrow={width === "narrow"} 
+  class:wide={width === "wide"}
+>
   <h4>
     {#if item.titleSplit !== undefined && width === "narrow"}
       {item.titleSplit[0]}<br/>{item.titleSplit[1]}
@@ -17,11 +20,11 @@
     {/if}
   </h4>
   {#if item.type === "compilation" || item.type === "series"}
-    <span class:remove={width === "narrow"}>&middot</span>
+    <span>&middot</span>
     <p>{item.books.length} books</p>
   {/if}
   {#if item.compilations !== undefined && item.compilations.length > 0}
-    <span class:remove={width === "narrow"}>&middot</span>
+    <span>&middot</span>
     <p>
       {item.compilations.length} 
       compilation{item.compilations.length > 1 ? "s" : ""}
@@ -31,7 +34,25 @@
 
 <!-- STYLES //////////////////////////////////////// -->
 <style>
-.remove {
+.narrow {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+  flex: 1;
+}
+.narrow h4 {
+  text-align: center;
+  line-height: 1.5;
+}
+.narrow span {
   display: none;
+}
+.wide {
+  display: flex;
+  align-items: center;
+  padding-top: 18px;
+  gap: 10px;
 }
 </style>
