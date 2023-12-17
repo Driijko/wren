bab4dd53586e2fdc1fdbd10260eb608234a9814a
Driijko <driijko@gmail.com>
2023-12-16 19:34:50 -0500

    CatalogueListItemHeader.

--- src/components/4-structures/catalogue/list/item/CatalogueListItemHeader.svelte
+++ src/components/4-structures/catalogue/list/item/CatalogueListItemHeader.svelte
@@ -1,4 +1,17 @@
 <!-- SCRIPTS ///////////////////////////////// -->
 <script>
-  
+  // PROPS -------------------------------------
+  export let context;
+  export let item;
+  export let width;
+
 </script>
+
+<!-- MARKUP //////////////////////////////////////// -->
+<header>
+  {#if item.titleSplit !== undefined && width === "narrow"}
+    <h4>{item.titleSplit[0]}<br/>{item.titleSplit[1]}</h4>
+  {:else}
+    <h4>{item.title}</h4>
+  {/if}
+</header>
