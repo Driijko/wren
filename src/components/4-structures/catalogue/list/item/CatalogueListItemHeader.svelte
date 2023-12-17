a4a21ab1b2e69fd28398b34c0b0b5a7712d59972
Driijko <driijko@gmail.com>
2023-12-16 19:55:57 -0500

    CatalogueListItemHeader: add conditional rendering.

--- src/components/4-structures/catalogue/list/item/CatalogueListItemHeader.svelte
+++ src/components/4-structures/catalogue/list/item/CatalogueListItemHeader.svelte
@@ -9,9 +9,29 @@
 
 <!-- MARKUP //////////////////////////////////////// -->
 <header>
-  {#if item.titleSplit !== undefined && width === "narrow"}
-    <h4>{item.titleSplit[0]}<br/>{item.titleSplit[1]}</h4>
-  {:else}
-    <h4>{item.title}</h4>
+  <h4>
+    {#if item.titleSplit !== undefined && width === "narrow"}
+      {item.titleSplit[0]}<br/>{item.titleSplit[1]}
+    {:else}
+      {item.title}
+    {/if}
+  </h4>
+  {#if item.type === "compilation" || item.type === "series"}
+    <span class:remove={width === "narrow"}>&middot</span>
+    <p>{item.books.length} books</p>
+  {/if}
+  {#if item.compilations !== undefined && item.compilations.length > 0}
+    <span class:remove={width === "narrow"}>&middot</span>
+    <p>
+      {item.compilations.length} 
+      compilation{item.compilations.length > 1 ? "s" : ""}
+    </p>
   {/if}
 </header>
+
+<!-- STYLES //////////////////////////////////////// -->
+<style>
+.remove {
+  display: none;
+}
+</style>
