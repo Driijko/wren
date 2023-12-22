78d2351f54a159b47437a3093ac1dd13a8484dde
Driijko <driijko@gmail.com>
2023-12-21 23:32:42 -0500

    Sort: add alphabetical sorting.

---
+++ src/components/1-site/interface/interfaces/Sort.svelte
@@ -0,0 +1,45 @@
+<!-- SCRIPTS //////////////////////////////////////// -->
+<script>
+  // IMPORTS ----------------------------------------------
+  import { catalogueMainType, setSort } 
+  from "../../../../dynamic/catalogueDisplay";
+  import { closeInterfaceModal } from "../../../../dynamic/interface";
+  import CatalogueMainTypeMenu 
+  from "../../../4-structures/catalogue/main/CatalogueMainTypeMenu.svelte";
+
+  // MAP TYPE TO STRING ----------------------------------
+  const map = { 
+    books: "Books", series: "Series", compilations: "Compilations"
+  };
+
+  // EVENT HANDLERS -----------------------------------
+  function handleClick(property, order) {
+    setSort($catalogueMainType, property, order);
+    closeInterfaceModal();
+  };
+
+</script>
+
+<!-- MARKUP ///////////////////////////////////////////// -->
+<CatalogueMainTypeMenu />
+<div>
+  <h2>Sort {map[$catalogueMainType]}:</h2>
+  <div>
+    <h3>Alphabetically</h3>
+    <button on:click={()=> handleClick("title", "start")}>
+      A - Z
+    </button>
+    <button on:click={()=> handleClick("title", "end")}>
+      Z - A
+    </button>
+  </div>
+  <div>
+    <h3>by Publication Date</h3>
+    <button on:click={()=> handleClick("date", "start")}>
+      newest - oldest
+    </button>
+    <button on:click={()=> handleClick("date", "end")}>
+      oldest - newest
+    </button>
+  </div>
+</div>
--- src/components/1-site/interface/levels/SiteInterface.svelte
+++ src/components/1-site/interface/levels/SiteInterface.svelte
@@ -4,6 +4,7 @@
   import { currentInterfaceSite } from "../../../../dynamic/interface";
   import SiteMenu from "../interfaces/SiteMenu.svelte";
   import Search from "../interfaces/Search.svelte";
+  import Sort from "../interfaces/Sort.svelte";
 
 </script>
 
@@ -14,5 +15,5 @@
 {:else if $currentInterfaceSite === "filter"}
   <p>Filter Products</p>
 {:else if $currentInterfaceSite === "sort"}
-  <p>Sort Products</p>
+  <Sort />
 {/if}
